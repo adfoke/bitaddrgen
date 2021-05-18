b5a0a94d10ccb33be59b2189e8f83b92926efc5f
adfoke <trawow@gmail.com>
2021-05-18 08:53:39 +0800

    add read file function
    add read file function.

--- addrgen.go
+++ addrgen.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
-	"math/big"
+	"os"
+   "bufio"
+   "math/big"
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
 )
 
 func main(){
+	file, err := os.Open("test.txt")
+	if err != nil {
+		panic(err)
+	 }
+	defer file.Close()
+	var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+	for _,line := range lines{
+	println("Hex:",line)
 	d := new(big.Int)
-	d.SetString("0x60f4d11574f5deee49961d9609ac6"[2:],16)
+	d.SetString(line[2:],16)
 	fmt.Println("Decimal:",d)
 	privKey, _ := btcec.NewPrivateKey(btcec.S256())
 	privKey.D = d
 	fmt.Println("privKey:",privKey.D)
 	privKeyWif, _ := btcutil.NewWIF(privKey, &chaincfg.MainNetParams, true)
 	fmt.Println("privKeyWif:",privKeyWif)
+	}
+	
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module addrgen.go
 go 1.16
 
 require (
-	github.com/Onther-Tech/go-ethereum v1.8.23
 	github.com/btcsuite/btcd v0.21.0-beta
 	github.com/btcsuite/btcutil v1.0.2
-	github.com/ethereum/go-ethereum v1.10.2 // indirect
 	golang.org/x/crypto v0.0.0-20210503195802-e9a32991a82e // indirect
 )
