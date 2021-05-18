312180d7d3e1e2a1c95c02a76229396c6de8ac0a
adfoke <trawow@gmail.com>
2021-05-18 11:33:45 +0800

    go fmt
    go fmt

--- addrgen.go
+++ addrgen.go
@@ -1,36 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"os"
-   "bufio"
-   "math/big"
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
+	"math/big"
+	"os"
 )
 
-func main(){
+func main() {
 	file, err := os.Open("test.txt")
 	if err != nil {
 		panic(err)
-	 }
+	}
 	defer file.Close()
 	var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-	for _,line := range lines{
-	println("Hex:",line)
-	d := new(big.Int)
-	d.SetString(line[2:],16)
-	fmt.Println("Decimal:",d)
-	privKey, _ := btcec.NewPrivateKey(btcec.S256())
-	privKey.D = d
-	fmt.Println("privKey:",privKey.D)
-	privKeyWif, _ := btcutil.NewWIF(privKey, &chaincfg.MainNetParams, true)
-	fmt.Println("privKeyWif:",privKeyWif)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
 	}
-	
+	for _, line := range lines {
+		println("Hex:", line)
+		d := new(big.Int)
+		d.SetString(line[2:], 16)
+		fmt.Println("Decimal:", d)
+		privKey, _ := btcec.NewPrivateKey(btcec.S256())
+		privKey.D = d
+		fmt.Println("privKey:", privKey.D)
+		privKeyWif, _ := btcutil.NewWIF(privKey, &chaincfg.MainNetParams, true)
+		fmt.Println("privKeyWif:", privKeyWif)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.16
 require (
 	github.com/btcsuite/btcd v0.21.0-beta
 	github.com/btcsuite/btcutil v1.0.2
-	golang.org/x/crypto v0.0.0-20210503195802-e9a32991a82e // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 )
