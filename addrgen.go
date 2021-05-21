a82d7d1d1edd5aec33543683501f45a2a02d1a20
adfoke <trawow@gmail.com>
2021-05-21 13:30:02 +0800

    add the save to file function
    add the save to file function

--- addrgen.go
+++ addrgen.go
@@ -3,11 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/big"
+	"os"
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
-	"math/big"
-	"os"
 )
 
 func main() {
@@ -21,16 +21,39 @@ func main() {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-	for _, line := range lines {
+	var savestring [1000]string
+	for idx, line := range lines {
 		println("Hex:", line)
 		d := new(big.Int)
-		d.SetString(line[2:], 16)
+		d.SetString(line, 16)
 		fmt.Println("Decimal:", d)
 		privKey, _ := btcec.NewPrivateKey(btcec.S256())
 		privKey.D = d
 		fmt.Println("privKey:", privKey.D)
 		privKeyWif, _ := btcutil.NewWIF(privKey, &chaincfg.MainNetParams, true)
-		fmt.Println("privKeyWif:", privKeyWif)
+		savestring[idx] = privKeyWif.String()
+		fmt.Println("privKeyWif:", savestring[idx])
 	}
-
+	var savefile ="save.txt"
+	var f *os.File
+	var err2 error
+	f, err2 = os.OpenFile(savefile, os.O_WRONLY|os.O_CREATE, 0666)
+	if err2 != nil {
+        fmt.Println("file open error", err2)
+    }
+	defer f.Close()
+	writer := bufio.NewWriter(f)
+	 for _,line := range savestring{
+		 writer.WriteString(line)		 
+		 writer.WriteString("\n")
+	}
+	writer.WriteString("haha")
+	writer.Flush() 
 }
+
+func checkFileIsExist(filename string) bool {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+	   return false
+	}
+	return true
+ }
